ea36fe81a559418e29ad60ce66fd6339270f902f
LEEGWONSEOK <leestart1023@gmail.com>
2023-01-24 00:41:21 +0900

    Feat: .env 환경 설정 진행

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,22 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeORMConfig } from './configs/typeorm.config';
 import { BoardsModule } from './boards/board.module';
 import { UsersModule } from './users/user.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeORMConfig), BoardsModule, UsersModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath:
+        process.env.NODE_ENV === 'production'
+          ? '.production.env'
+          : process.env.NODE_ENV === 'stage'
+          ? '.stage.env'
+          : '.development.env',
+    }),
+    TypeOrmModule.forRoot(typeORMConfig),
+    BoardsModule,
+    UsersModule,
+  ],
   providers: [],
 })
 export class AppModule {}
